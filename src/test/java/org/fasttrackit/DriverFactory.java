017e9ec619c69a74d871c2d11776686d55b112d7
mury7 <cipriancluj1976@gmail.com>
2018-06-14 12:10:00 +0300

    Added more drivers: firefox, ie, edge, opera, and created a switch in DriverFactory to be able to choose the browser when running tests in command prompt.

--- src/test/java/org/fasttrackit/DriverFactory.java
+++ src/test/java/org/fasttrackit/DriverFactory.java
@@ -2,6 +2,10 @@ package org.fasttrackit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.opera.OperaDriver;
 
 import java.util.concurrent.TimeUnit;
 
@@ -10,8 +14,41 @@ public class DriverFactory {
     private static WebDriver driver;
 
     public static void initDriver() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\mury7\\FastTrackIt\\nadisonIslandTest\\src\\test\\resources\\drivers\\chromedriver.exe");
-        driver = new ChromeDriver();
+        // Reading parameter provided from command line
+        // For example: mvn test -Dtest="testname" -Dbrowser=firefox
+
+        String browser = System.getProperty("browser", "chrome");
+
+        switch (browser) {
+            default:
+                System.out.println("Using default browser: Chrome");
+            case "chrome":
+                System.setProperty("webdriver.chrome.driver",
+                        "C:\\Users\\mury7\\FastTrackIt\\nadisonIslandTest\\src\\test\\resources\\drivers\\chromedriver.exe");
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                System.setProperty("webdriver.gecko.driver",
+                        "C:\\Users\\mury7\\FastTrackIt\\madisonIslandTestJunit\\src\\test\\resources\\drivers\\geckodriver.exe");
+                driver = new FirefoxDriver();
+                break;
+            case "ie":
+                System.setProperty("webdriver.ie.driver",
+                        "C:\\Users\\mury7\\FastTrackIt\\madisonIslandTestJunit\\src\\test\\resources\\drivers\\IEDriverServer.exe");
+                driver = new InternetExplorerDriver();
+                break;
+            case "opera":
+                System.setProperty("webdriver.opera.driver",
+                        "C:\\Users\\mury7\\FastTrackIt\\madisonIslandTestJunit\\src\\test\\resources\\drivers\\operadriver.exe");
+                driver = new OperaDriver();
+                break;
+            case "edge":
+                System.setProperty("webdriver.edge.driver",
+                        "C:\\Users\\mury7\\FastTrackIt\\madisonIslandTestJunit\\src\\test\\resources\\drivers\\MicrosoftWebDriver.exe");
+                driver = new EdgeDriver();
+                break;
+        }
+
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.manage().window().maximize();
         DriverFactory.getDriver().get("https://fasttrackit.org/selenium-test/");
--- src/test/java/org/fasttrackit/tests/LoginTest.java
+++ src/test/java/org/fasttrackit/tests/LoginTest.java
@@ -4,14 +4,16 @@ import org.fasttrackit.DriverFactory;
 import org.fasttrackit.steps.LoginFormSteps;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class LoginTest extends TestBase {
 
     @Test
-    public void tryToLoginWithValidData () {
+    public void tryToLoginWithValidData() {
         LoginFormSteps goToLogin = new LoginFormSteps();
         goToLogin.goToLoginPage();
 
@@ -26,7 +28,7 @@ public class LoginTest extends TestBase {
     }
 
     @Test
-    public void tryToLoginWithNonExistentAccount () {
+    public void tryToLoginWithNonExistentAccount() {
         LoginFormSteps goToLogin = new LoginFormSteps();
         goToLogin.goToLoginPage();
 
@@ -41,7 +43,7 @@ public class LoginTest extends TestBase {
     }
 
     @Test
-    public void tryToLoginWithEmailFieldEmpty () {
+    public void tryToLoginWithEmailFieldEmpty() {
         LoginFormSteps goToLogin = new LoginFormSteps();
         goToLogin.goToLoginPage();
 
@@ -56,7 +58,7 @@ public class LoginTest extends TestBase {
     }
 
     @Test
-    public void tryToLoginWithPasswordFieldEmpty () {
+    public void tryToLoginWithPasswordFieldEmpty() {
         LoginFormSteps goToLogin = new LoginFormSteps();
         goToLogin.goToLoginPage();
 
@@ -68,6 +70,25 @@ public class LoginTest extends TestBase {
 
         WebElement errorMsgContainer = DriverFactory.getDriver().findElement(By.id("advice-required-entry-pass"));
         assertThat("The user logged in without entering the email", errorMsgContainer.isDisplayed());
+    }
+
+    @Test
+    public void tryToLoginWithoutAtInEmailField() {
+        LoginFormSteps goToLogin = new LoginFormSteps();
+        goToLogin.goToLoginPage();
+
+        String email = "mailmail.com";
+        String password = "123456";
+
+        LoginFormSteps loginFormSteps = new LoginFormSteps();
+        loginFormSteps.fillOutTheLoginPage(email, password);
+
+        JavascriptExecutor js = (JavascriptExecutor)DriverFactory.getDriver();
+        WebElement field = DriverFactory.getDriver().findElement(By.id("email"));
+        Boolean is_valid = (Boolean)js.executeScript("return arguments[0].checkValidity();", field);
+
+        assertThat("The email field validates email without @ !", is_valid, is(false));
+        DriverFactory.getDriver().quit();
     }
 
 }
